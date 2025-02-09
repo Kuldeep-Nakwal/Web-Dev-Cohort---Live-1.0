f5e730879eb9061e25c5d51a90ac9a0834001cdc
Kuldeep Nakwal <knakwal@gmail.com>
2025-02-09 19:25:44 +0000

    Learning Basic function

---
+++ First_Class/hello.js
@@ -0,0 +1,11 @@
+function printChai() {
+    console.log("Hello Chai !");
+}
+
+
+function bringBrush(number) {
+    console.log(`Hanji Le aaya ${number} Brush.`);   
+}
+
+printChai();
+bringBrush(400);
